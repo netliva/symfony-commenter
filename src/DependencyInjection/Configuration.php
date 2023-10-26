613e3b3c91cddbbb7b03daab4281faaa974d3d5d
Bilal YILMAZ <mail@bilalyilmaz.com>
2023-10-27 00:52:56 +0300

    hotfix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root(' netliva_comment');
+        $rootNode = $treeBuilder->root('netliva_comment');
 
 		$rootNode
 			->children()
